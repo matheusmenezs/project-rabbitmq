33d35900376eefe939a9056122f06cb980699a74
Matheus Menezes <menezes@pdsolucoes.com.br>
2023-04-23 21:25:29 -0300

    style: format rabbit interface code

--- src/rabbitmq/IRabbit.ts
+++ src/rabbitmq/IRabbit.ts
@@ -1,22 +1,29 @@
 import amqp from 'amqplib';
 
 export interface IRabbitMQ {
-    connect(): Promise<void>;
-    assertQueue(queue: string, options: amqp.Options.AssertQueue): Promise<string>;
-    sendToQueue(queue: string, message: string, options: amqp.Options.Publish): Promise<void>;
-    consumeFromQueue(queue: string): Promise<void>;
-    sendToExchange(
-        exchange: string,
-        routingKey: string,
-        message: string,
-        options: amqp.Options.Publish
-    ): Promise<void>;
-    consumeFromExchange(
-        queue: string,
-        exchange: string,
-        routingKey: string,
-        callback: (msg: amqp.ConsumeMessage) => void,
-        options: amqp.Options.Consume
-    ): Promise<void>;
-    close(): Promise<void>;
+  connect(): Promise<void>;
+  assertQueue(
+    queue: string,
+    options: amqp.Options.AssertQueue
+  ): Promise<string>;
+  sendToQueue(
+    queue: string,
+    message: string,
+    options: amqp.Options.Publish
+  ): Promise<void>;
+  consumeFromQueue(queue: string): Promise<void>;
+  sendToExchange(
+    exchange: string,
+    routingKey: string,
+    message: string,
+    options: amqp.Options.Publish
+  ): Promise<void>;
+  consumeFromExchange(
+    queue: string,
+    exchange: string,
+    routingKey: string,
+    callback: (msg: amqp.ConsumeMessage) => void,
+    options: amqp.Options.Consume
+  ): Promise<void>;
+  close(): Promise<void>;
 }
